941f10443b3659e1bffc7d96a6f3aa1965ef9a72
Your Name <you>
2024-04-10 08:34:38 +0000

    Alsomt ready

--- client.c
+++ client.c
@@ -9,9 +9,126 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <openssl/dh.h>
+#include <openssl/evp.h>
 
 #define MAX_DATA_SIZE 80
+#define KEY_SIZE 16
 
+int crypto = 0;
+unsigned char *session_key;
+size_t session_key_len;
+
+
+void print_secret_key(const unsigned char *key, size_t key_len) {
+    printf("Secret key (hex): ");
+    for (size_t i = 0; i < key_len; ++i) {
+        printf("%02x", key[i]);
+    }
+    printf("\n");
+}
+
+void generate_session_key(int *sock) {
+
+    // Creating dh context with hardcoded p and g
+    EVP_PKEY_CTX *dh_params_ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_DH, NULL);
+    if (!dh_params_ctx) {
+        perror("Ошибка при создании контекста параметров домена");
+        exit(EXIT_FAILURE);
+    }
+
+    int g = 7;
+    int p = 103;
+
+    if (EVP_PKEY_CTX_set_dh_paramgen_prime_len(dh_params_ctx, 1024) <= 0) {
+        perror("Ошибка при установке параметров домена");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY *params = NULL;
+    if (EVP_PKEY_paramgen(dh_params_ctx, &params) <= 0) {
+        perror("Ошибка при генерации параметров домена");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY_CTX *keygen_ctx = EVP_PKEY_CTX_new(params, NULL);
+    if (!keygen_ctx) {
+        perror("Ошибка при создании контекста ключевой пары");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY *keypair = NULL;
+    if (EVP_PKEY_keygen_init(keygen_ctx) <= 0 ||
+        EVP_PKEY_keygen(keygen_ctx, &keypair) <= 0) {
+        perror("Ошибка при генерации ключевой пары");
+        exit(EXIT_FAILURE);
+    }
+
+    // Get server public key
+    size_t other_pub_key_len = 0;
+    if (recv(*sock, &other_pub_key_len, sizeof(size_t), 0) < 0) {
+        perror("Ошибка при получении длины публичного ключа другой стороны");
+        exit(EXIT_FAILURE);
+    }
+    unsigned char *other_pub_key_data = malloc(other_pub_key_len);
+    if (!other_pub_key_data ||
+        recv(*sock, other_pub_key_data, other_pub_key_len, 0) < 0) {
+        perror("Ошибка при получении публичного ключа другой стороны");
+        exit(EXIT_FAILURE);
+    }
+
+    EVP_PKEY *other_pub_key = EVP_PKEY_new_raw_public_key(EVP_PKEY_DH, NULL,
+                                                           other_pub_key_data,
+                                                           other_pub_key_len);
+    if (!other_pub_key) {
+        perror("Ошибка при создании публичного ключа другой стороны");
+        exit(EXIT_FAILURE);
+    }
+
+    // Generating and sending public client key
+    unsigned char *pub_key_data = NULL;
+    size_t pub_key_len = 0;
+    if (EVP_PKEY_get_raw_public_key(keypair, NULL, &pub_key_len) <= 0 ||
+        !(pub_key_data = malloc(pub_key_len)) ||
+        EVP_PKEY_get_raw_public_key(keypair, pub_key_data, &pub_key_len) <= 0 ||
+        send(*sock, &pub_key_len, sizeof(size_t), 0) < 0 ||
+        send(*sock, pub_key_data, pub_key_len, 0) < 0) {
+        perror("Ошибка при отправке публичного ключа");
+        exit(EXIT_FAILURE);
+    }
+    free(pub_key_data);
+
+    EVP_PKEY_CTX *derive_ctx = EVP_PKEY_CTX_new(keypair, NULL);
+    if (!derive_ctx) {
+        perror("Ошибка при создании контекста для вычисления общего секрета");
+        exit(EXIT_FAILURE);
+    }
+
+    if (EVP_PKEY_derive_init(derive_ctx) <= 0 ||
+        EVP_PKEY_derive_set_peer(derive_ctx, other_pub_key) <= 0 ||
+        EVP_PKEY_derive(derive_ctx, NULL, &session_key_len) <= 0) {
+        perror("Ошибка при вычислении общего секрета");
+        exit(EXIT_FAILURE);
+    }
+
+    // Generate session key
+    session_key = malloc(session_key_len);
+    if (! session_key ||
+        EVP_PKEY_derive(derive_ctx, session_key, &session_key_len) <= 0) {
+        perror("Ошибка при вычислении общего секрета");
+        exit(EXIT_FAILURE);
+    }
+
+    print_secret_key(session_key, session_key_len);
+
+    // free memory
+    EVP_PKEY_free(params);
+    EVP_PKEY_free(keypair);
+    EVP_PKEY_free(other_pub_key);
+    EVP_PKEY_CTX_free(dh_params_ctx);
+    EVP_PKEY_CTX_free(keygen_ctx);
+    EVP_PKEY_CTX_free(derive_ctx);
+}
 
 void send_message(int *sock)
 {
@@ -56,12 +173,6 @@ int recv_message(int *sock)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        printf("Usage: %s hostname port", basename(argv[0]));
-        exit(-1);
-    }
-
     char* hostname = argv[1];
     char* port = argv[2];
     int port_int = atoi(port);
@@ -70,6 +181,7 @@ int main(int argc, char *argv[])
 
     struct sockaddr_in addr;
 
+    // socker for connection to server
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sock < 0) {
@@ -81,17 +193,32 @@ int main(int argc, char *argv[])
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port_int);
 
-    // hostname в структуру in_addr
+    // hostname at structure in_addr
     if (inet_pton(AF_INET, hostname, &addr.sin_addr) <= 0) {
         perror("inet_pton");
         exit(-1);
     }
 
+    // connect with server
     if (connect(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
 
         perror("connect");
         exit(-1);
 
+    }
+
+    // get info about encryption from client
+    if (recv(sock, &crypto, MAX_DATA_SIZE-1, 0) == -1)
+    {
+        perror("recv");
+    }
+
+    crypto = ntohl(crypto);
+
+    // Generation key for crypto mode
+    if (crypto) {
+       generate_session_key(&sock);
+       printf("Ключ шифрования был успешно сгенерирован\n");
     }
 
     fd_set read_fds;
--- server.c
+++ server.c
@@ -9,8 +9,110 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <openssl/dh.h>
+#include <openssl/evp.h>
 
 #define MAX_DATA_SIZE 80
+#define KEY_SIZE 16
+
+int crypto = 0;
+unsigned char *session_key;
+size_t session_key_len;
+
+void generate_session_key(int *sock) {
+
+    EVP_PKEY_CTX *kctx = NULL;
+    EVP_PKEY *dhkey = NULL;
+    EVP_PKEY *params = NULL;
+    BIO* fp = NULL;
+
+    if (!(params = EVP_PKEY_new())) {
+        fprintf(stderr, "Error for params creating\n");
+        exit(-1);
+    }
+
+    if (1 != EVP_PKEY_assign(params, EVP_PKEY_DHX, DH_get_2048_256())) {
+        fprintf(stderr, "Error in assign\n");
+        exit(-1);
+    }
+
+    if (!(kctx = EVP_PKEY_CTX_new(params, NULL))) {
+        fprintf(stderr, "Error in initializing kctx\n");
+        exit(-1);
+    }
+
+    if (1 != EVP_PKEY_keygen_init(kctx)) {
+        fprintf(stderr, "Error in key generation\n");
+        exit(-1);
+    }
+
+    if (1 != EVP_PKEY_keygen(kctx, &dhkey)) {
+        fprintf(stderr, "Error in key generation\n");
+        exit(-1);
+    }
+
+    fp = BIO_new_fp(stdout, BIO_NOCLOSE);
+    if (fp == NULL) {
+        fprintf(stderr, "Error creating BIO\n");
+        exit(-1);
+    }
+
+    EVP_PKEY_print_private(fp, dhkey, 0, NULL);
+
+    DH *dh = EVP_PKEY_get1_DH(dhkey);
+    if (dh == NULL) {
+        fprintf(stderr, "Error getting DH structure\n");
+        exit(-1);
+    }
+
+    const BIGNUM *pub_key_bn;
+    DH_get0_key(dh, &pub_key_bn, NULL);
+    unsigned char *pub_key_bytes = (unsigned char *)malloc(DH_size(dh));
+    if (pub_key_bytes == NULL) {
+        fprintf(stderr, "Error allocating memory\n");
+        exit(-1);
+    }
+
+    int pub_key_len = BN_bn2bin(pub_key_bn, pub_key_bytes);
+
+    send(*sock, pub_key_bytes, pub_key_len, 0);
+    printf("Public key sent to client\n");
+
+    unsigned char client_public_key[1024];
+    int bytes_received = recv(*sock, client_public_key, sizeof(client_public_key), 0);
+    if (bytes_received < 0) {
+        fprintf(stderr, "Error receiving client's public key\n");
+        exit(-1);
+    }
+
+    if (1 != EVP_PKEY_derive_init(kctx)) {
+        fprintf(stderr, "Error deriving\n");
+        exit(-1);
+    }
+
+    if (1 != EVP_PKEY_derive_set_peer(kctx, client_public_key)) {
+        fprintf(stderr, "Error in settning client public key\n");
+        exit(-1);
+    }
+
+    if (1 != EVP_PKEY_derive(kctx, NULL, &session_key_len)) {
+        fprintf(stderr, "Error deriving\n");
+        exit(-1);
+    }
+
+    session_key = (unsigned char *)malloc(session_key_len);
+
+    if (1 != (EVP_PKEY_derive(kctx, session_key, &session_key_len))) {
+        fprintf(stderr, "Error in creating session key\n");
+        exit(-1);
+    }
+
+    EVP_PKEY_free(params);
+    EVP_PKEY_CTX_free(kctx);
+    EVP_PKEY_free(dhkey);
+    BIO_free(fp);
+
+}
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
@@ -33,6 +135,7 @@ void send_message(int *sock)
     sendbuf[msg_length] = '\0';
     msg_length += 1;
 
+    // Check that message has valid size
     if (msg_length >= MAX_DATA_SIZE) {
         printf("The message is too long\n");
         return;
@@ -65,19 +168,16 @@ int recv_message(int *sock)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Usage: %s port", basename(argv[0]));
-        exit(-1);
-    }
 
     char* port = argv[1];
+    crypto = 1;
     int port_int = atoi(port);
 
 
     struct sockaddr_in addr;
     int yes = 1;
 
+    // Listener socker for 0.0.0.0 with port from parametres
     int listener = socket(AF_INET, SOCK_STREAM, 0);
 
     if (listener < 0) {
@@ -89,8 +189,10 @@ int main(int argc, char *argv[])
     addr.sin_port = htons(port_int);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    // Release listener
     setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
 
+    // bind listener for external connections
     if (bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
 
        perror("Failed to bind listener");
@@ -114,6 +216,7 @@ int main(int argc, char *argv[])
         struct sockaddr_storage remoteaddr;
         socklen_t addrlen = sizeof(remoteaddr);
 
+        // waiting and accepting client
         client_sock = accept(listener, (struct sockaddr *)&remoteaddr, &addrlen);
         if (client_sock == -1)
         {
@@ -121,11 +224,27 @@ int main(int argc, char *argv[])
             continue;
         }
 
+        // Get information about client
         char remoteIP[INET6_ADDRSTRLEN];
         inet_ntop(remoteaddr.ss_family, get_in_addr((struct sockaddr *)&remoteaddr), remoteIP, sizeof(remoteIP));
         printf("Connection established with %s\n", remoteIP);
+
+        //Close listener, work with only one client
         close(listener);
+
+        // Send info about encryption to client
+        int crypto_info = htonl(crypto);
+    	if (send(client_sock, &crypto_info, sizeof(crypto_info), 0) < 0) {
+            perror("Ошибка при отправке данных");
+            exit(-1);
+        }
         break;
+    }
+
+    // Generetion key for crypto mode
+    if (crypto) {
+        generate_session_key(&client_sock);
+        printf("Ключ шифрования был успешно сгенерирован\n");
     }
 
     fd_set read_fds;
@@ -139,14 +258,14 @@ int main(int argc, char *argv[])
     while (1) {
         fd_set temp_fds = read_fds;
 
-        // Бесконечно ждем активности
+        // Waiting activity from stdin or client socket, select provides duplex communication
         int activity = select(max_fd + 1, &temp_fds, NULL, NULL, NULL);
         if (activity < 0) {
             perror("Error in select");
             exit(EXIT_FAILURE);
         }
 
-        // Проверяем сообщение от клиента
+        // Check message from client
         if (FD_ISSET(client_sock, &temp_fds)) {
             if (! recv_message(&client_sock)) {
                 close(client_sock);
@@ -154,7 +273,7 @@ int main(int argc, char *argv[])
             }
         }
 
-        // Проверяем поток stdin
+        // Check stdin to sending message for client
         if (FD_ISSET(STDIN_FILENO, &temp_fds)) {
             send_message(&client_sock);
         }
